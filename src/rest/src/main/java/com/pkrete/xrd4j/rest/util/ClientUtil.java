90deaab3262fa82af57cfe72cafb7131d1e55651
Samuli Lehto <samuli.lehto@iki.fi>
2017-07-07 15:29:58 +0300

    Added url encoding of parameter values when building the query string of an url.

--- src/rest/src/main/java/com/pkrete/xrd4j/rest/util/ClientUtil.java
+++ src/rest/src/main/java/com/pkrete/xrd4j/rest/util/ClientUtil.java
@@ -3,6 +3,7 @@ package com.pkrete.xrd4j.rest.util;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URLEncoder;
 import java.util.List;
 import java.util.Map;
 import org.apache.http.HttpEntity;
@@ -120,6 +121,11 @@ public class ClientUtil {
         }
         // Remove line breaks and omit leading and trailing whitespace
         value = value.replaceAll(REMOVE_WHITE_SPACE_PATTERN, "").trim();
+        try {
+            value = URLEncoder.encode(value, "UTF-8");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         paramsString.append(name).append("=").append(value);
         logger.debug("Parameter : \"{}\"=\"{}\"", name, value);
     }
