556187d6c4aa9e319c0ae0b49ba44ae56c3f2c89
Samuli Lehto <samuli.lehto@iki.fi>
2017-07-07 15:31:00 +0300

    Added automatic detection and using of a system proxy (probably works only in Windows).

--- src/rest/src/main/java/com/pkrete/xrd4j/rest/client/AbstractClient.java
+++ src/rest/src/main/java/com/pkrete/xrd4j/rest/client/AbstractClient.java
@@ -1,14 +1,25 @@
 package com.pkrete.xrd4j.rest.client;
 
+import com.github.markusbernhardt.proxy.ProxySearch;
+import com.github.markusbernhardt.proxy.selector.misc.BufferedProxySelector;
 import com.pkrete.xrd4j.rest.ClientResponse;
 import com.pkrete.xrd4j.rest.util.ClientUtil;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import org.apache.http.Header;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
+import org.apache.http.HttpHost;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,6 +34,49 @@ public abstract class AbstractClient implements RESTClient {
     private static final Logger logger = LoggerFactory.getLogger(AbstractClient.class);
 
     protected abstract HttpUriRequest buildtHttpRequest(String url, String requestBody, Map<String, String> headers);
+
+    // The proxy detection code is taken from:
+    // https://stackoverflow.com/questions/4933677/detecting-windows-ie-proxy-setting-using-java
+    public HttpHost getProxyHost(String url) {
+        try {
+            System.setProperty("java.net.useSystemProxies", "true");
+
+            // Use proxy vole to find the default proxy
+            ProxySearch ps = ProxySearch.getDefaultProxySearch();
+
+            ps.setPacCacheSettings(32, 1000 * 60 * 5, BufferedProxySelector.CacheScope.CACHE_SCOPE_URL);
+            logger.info("Selecting proxy");
+            ProxySelector proxySelector = ps.getProxySelector();
+            if (proxySelector == null) {
+                logger.info("Proxyselector is null, not using proxy");
+                return null;
+            }
+            List<Proxy> l = proxySelector.select(new URI(url));
+            logger.info("Proxy list; {}", l);
+            Iterator<Proxy> iter = l.iterator();
+
+            if (!iter.hasNext()) {
+                logger.info("No Proxy found");
+                return null;
+            } else {
+                Proxy proxy = iter.next();
+
+                logger.info("proxy type : {}", proxy.type());
+                InetSocketAddress addr = (InetSocketAddress) proxy.address();
+
+                if (addr == null) {
+                    logger.info("No Proxy (addess is null)");
+                    return null;
+                } else {
+                    logger.info("proxy hostname : {}", addr.getHostName());
+                    logger.info("proxy port : {}", addr.getPort());
+                    return new HttpHost(addr.getHostName(), addr.getPort());
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     /**
      * Makes a HTTP request to the given URL using the given request body,
@@ -42,7 +96,14 @@ public abstract class AbstractClient implements RESTClient {
         url = ClientUtil.buildTargetURL(url, params);
 
         // Create HTTP client
-        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpClientBuilder httpClientBuilder = HttpClients.custom();
+
+        HttpHost proxy = getProxyHost(url);
+        if (proxy != null) {
+            DefaultProxyRoutePlanner routePlanner = new DefaultProxyRoutePlanner(proxy);
+            httpClientBuilder.setRoutePlanner(routePlanner);
+        }
+        CloseableHttpClient httpClient = httpClientBuilder.build();
 
         // Build request
         HttpUriRequest request = this.buildtHttpRequest(url, requestBody, headers);
